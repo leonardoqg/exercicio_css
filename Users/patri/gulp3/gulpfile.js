697a461c0ac33150cdc8c5298c2d49c041f52e88
leonardoq <leonardoqgaldino@gmail.com>
2025-06-10 18:56:39 -0300

    Adiciona Gulpfile e configuração inicial

---
+++ Users/patri/gulp3/gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+gulp.task('sass', function () {
+    return gulp.src('src/sass/**/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('images', function () {
+    return gulp.src('src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('src/js/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', gulp.parallel('sass', 'images', 'scripts'));
